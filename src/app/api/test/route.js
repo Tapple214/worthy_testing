c867286aebded8fcc92f36865bdd0765eeea3ab8
M-A-C255 <zaypainglatt04@gmail.com>
2024-02-12 18:36:19 +0800

    updated get api

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -1,31 +1,14 @@
-// // Assuming you have already initialized PrismaClient
-// const { PrismaClient } = require('@prisma/client');
-// const prisma = new PrismaClient();
+import {NextResponse, nextResponse} from "next/server"
+import prisma from "@/lib/prisma"
 
-// // Define your API endpoint
-// app.get('/api/products', async (req, res) => {
-//   try {
-//     const products = await prisma.product.findMany({
-//       where: {
-//         posted: true
-//       },
-//       include: {
-//         user: {
-//           select: { user_name: true, assets: true, likes: true, supporters: true, pickup_location: true, rank: true, date_joined: true }
-//         }
-//       }
-//     });
-//     res.json(products);
-//   } catch (error) {
-//     console.error("Error fetching products:", error);
-//     res.status(500).json({ error: 'Internal Server Error' });
-//   }
-// });
+export async function GET() {
 
-import { NextResponse } from 'next/server';
+    const products = await prisma.product.findMany({
+        where: {
+            posted: true
+        }
+        })
 
-export async function GET(request) {
-    const res = await request.json();
-    console.log({res});
-    return NextResponse.json({data:res});
+    return NextResponse.json({products});
+
 }
