83906641ee944fc34e4cd4c3779226813e85e012
M-A-C255 <zaypainglatt04@gmail.com>
2024-02-12 19:43:21 +0800

    Added fetch api

--- src/app/api/test/route.js
+++ src/app/api/test/route.js
@@ -6,8 +6,13 @@ export async function GET() {
     const products = await prisma.product.findMany({
         where: {
             posted: true
-        }
-        })
+          },
+          include: {
+            user: {
+              select: { user_name: true, assets: true, likes: true, supporters: true, pickup_location: true, rank: true, date_joined: true }
+            }
+          }
+    })
 
     return NextResponse.json({products});
 
--- src/app/test/page.js
+++ src/app/test/page.js
@@ -3,41 +3,12 @@ import React, { useState, useEffect } from 'react';
 import '@/app/test/page.css';
 import Card from "@/app/components/card";
 import TinderCard from 'react-tinder-card';
-import prisma from "@/lib/prisma";
 
-// async function getProducts() {
-//   try {
-//     const products = await prisma.product.findMany({
-//       where: {
-//         posted: true
-//       },
-//       include: {
-//         user: {
-//           select: { user_name: true, assets: true, likes: true, supporters: true, pickup_location: true, rank: true, date_joined: true }
-//         }
-//       }
-//     });
-//     return products;
-//   } catch (error) {
-//     console.error("Error fetching products:", error);
-//     return [];
-//   }
-// }
 
 export default function App() {
-  const [products, setProducts] = useState([]);
+
   const [lastDirection, setLastDirection] = useState();
 
-  const [title, setTitle] = useState('');
-
-  // useEffect(() => {
-  //   const fetchProducts = async () => {
-  //     const fetchedProducts = await getProducts();
-  //     setProducts(fetchedProducts);
-  //   };
-
-  //   fetchProducts();
-  // }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
   const swiped = (direction, nameToDelete) => {
     console.log('deleting', nameToDelete);
@@ -48,29 +19,43 @@ export default function App() {
     console.log('out of frame ', name);
   };
 
-  try{
-    fetch('/api/test', {
-      method: 'GET', 
-      headers: {
-      'Content-Type': 'application/json'    
-    }
-    // body: JSON.stringify({title})
-    })
-  } catch(error){
-  console.error(error)
-  }
+  const [products, setProducts] = useState([])
+  const [isLoading, setLoading] = useState(true)
+  
+  useEffect(() => {
+    fetch('../api/test')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data.products)
+        setLoading(false)
+      })
+  }, [])
+
+  if (isLoading) return <p>Loading...</p>
+  if (!products) return <p>No profile data</p>
 
   return (
     <div className='dashboard'>
       <div className='swiper-container'>
+
         <div className='card-container'>
-          {products.map((product) => (
-            <TinderCard className='swipe' key={character.id} onSwipe={(direction) => swiped(direction,character.title)} onCardLeft>
-              <div style={{ backgroundImage: `url(${character.imageSrc})` }} className='card'>
-                  <h3>{character.title}</h3>
-              </div>
-          </TinderCard>
-          ))}
+
+          {
+            products.map((product) => (
+              <TinderCard 
+              key={product.product_id} 
+              className="swipe" 
+              onSwipe={(direction) => swiped(direction,product.product_name)} 
+              onCardLeft>
+                
+                <div style={{ backgroundImage: 'pp2.PNG' }} className='card'>
+                  <h3>{product.product_name}</h3>
+               </div>
+              
+              </TinderCard>
+            ))
+          }
+
         </div>
       </div>
 
